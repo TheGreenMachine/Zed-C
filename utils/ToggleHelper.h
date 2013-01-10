b71c3b86084f771ca04d44249e450f13c305ce05
Danny Gratzer <danny.gratzer@gmail.com>
2013-01-09 18:32:35 -0600

    Added GamePad utilities from last years code

---
+++ utils/ToggleHelper.h
@@ -0,0 +1,37 @@
+#ifndef TOGGLE_HELPER
+#define TOGGLE_HELPER
+/*
+ * ToggleHelper is a class designed to support a true toggle button, ie hit once to turn on and once
+ * to turn off.
+ */
+class ToggleHelper{
+public:
+	/*
+	 * Constructs ToggleHelper with toggleReady equal to false.
+	 */
+	ToggleHelper()
+	{
+		toggleReady = false;
+	}
+	/*
+	 * Returns true if the latests state is both true and different from toggle ready.
+	 */
+	bool operator()(bool latestState)
+	{
+		if(!latestState){
+		            toggleReady = true;
+	        }
+
+	        else if(latestState && toggleReady){
+	            toggleReady = false;
+	            return true;
+	        }
+	        else{
+	            toggleReady = false;
+	        }
+	        return false;
+	}
+private:
+	bool toggleReady;
+};
+#endif
