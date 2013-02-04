c0d91ebae7223f9e609e3db57d1c4404cb778a9a
Danny Gratzer <danny.gratzer@gmail.com>
2013-02-04 17:21:25 -0600

    Adjusted my scripts. Feel free to ignore

---
+++ space2tab.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "os"
+  "bytes"
+  "io/ioutil"
+  "path/filepath"
+)
+
+func main(){
+  replace, old := []byte{'\t'}, []byte{' ',' '}
+  files := []string{}
+  for _, name := range os.Args[1:] {
+    matches, _ := filepath.Glob(name)
+    files = append(files, matches...)
+  }
+  for _, file := range files {
+    if file == "space2tab.go" || file == "tab2space.go" {
+      continue
+    }
+    contents, _ := ioutil.ReadFile(file)
+    result := bytes.Replace(contents, old, replace, -1)
+    ioutil.WriteFile(file, result, 0644)
+  }
+}
